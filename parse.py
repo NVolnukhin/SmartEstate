393a4bdc200646e8d04abf70743c1784a76c421c
Nikita <volnukhinnd@student.bmstu.ru>
2025-03-27 19:01:59 +0300

    Update 14 files
    - /smart_estate_parser/definers/__init__.py
    - /smart_estate_parser/definers/definer_cities_id.py
    - /smart_estate_parser/definers/definer_metro_id.py
    - /smart_estate_parser/flat/__pycache__/list.cpython-312.pyc
    - /smart_estate_parser/flat/__pycache__/page.cpython-312.pyc
    - /smart_estate_parser/flat/list.py
    - /smart_estate_parser/flat/page.py
    - /smart_estate_parser/__init__.py
    - /smart_estate_parser/base_list.py
    - /smart_estate_parser/constants.py
    - /smart_estate_parser/proxy_pool.py
    - /smart_estate_parser/smart_estate_parser.py
    - /smart_estate_parser/url_builder.py
    - /parse.py

--- parse.py
+++
@@ -1,9 +0,0 @@
-import  smart_estate_parser
-
-moscow_parser = smart_estate_parser.SmartEstateParser(location="Москва")
-data = moscow_parser.get_flats(rooms="all", deal_type="sale", district=78, with_saving_csv=False)
-
-print(data[0])
-
-#Метро Некрасовка 373
-#Район Некрасовка 78
