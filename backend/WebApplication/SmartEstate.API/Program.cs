72d70b7fec40a6defcdc32f85773e583388752f6
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-01 22:27:49 +0300

    added SwaggerUI

--- backend/WebApplication/SmartEstate.API/Program.cs
+++ backend/WebApplication/SmartEstate.API/Program.cs
@@ -1,7 +1,15 @@
 using DatabaseContext;
 using Microsoft.EntityFrameworkCore;
+using SmartEstate.Routing.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRoutingModule(builder.Configuration);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
@@ -10,6 +18,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = "swagger";
+});
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.MapGet("/", () => "Hello World! Navigation to /swagger for API documentation");
 
 app.Run();
