3d31fd10414ca401ec5c0cb8bb614f01977a92c7
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-02 21:23:26 +0300

    update infrstructure info entity

--- backend/WebApplication/SmartEstate.DatabaseModel/Entities/InfrastructureInfo.cs
+++ backend/WebApplication/SmartEstate.DatabaseModel/Entities/InfrastructureInfo.cs
@@ -1,5 +1,35 @@
-﻿namespace DatabaseModel;
+﻿using DatabaseModel.Infrastucture;
 
+namespace DatabaseModel;
+
+
+public class InfrastructureInfo
+{
+    public int? InfrastructureInfoId { get; set; }
+    public string Facilities { get; set; } = string.Empty;
+    
+    // Navigation properties to nearest infrastructure
+    public int? NearestShopId { get; set; }
+    public int? NearestMetroId { get; set; }
+    public int? NearestSchoolId { get; set; }
+    public int? NearestKindergartenId { get; set; }
+    public int? NearestPharmacyId { get; set; }
+    
+    public int? MinutesToShop { get; set; }
+    public int? MinutesToMetro { get; set; }
+    public int? MinutesToSchool { get; set; }
+    public int? MinutesToKindergarten { get; set; }
+    public int? MinutesToPharmacy { get; set; }
+    
+    // Navigation properties
+    public Building? Building { get; set; }
+    public Shop? NearestShop { get; set; }
+    public Metro? NearestMetro { get; set; }
+    public School? NearestSchool { get; set; }
+    public Kindergarten? NearestKindergarten { get; set; }
+    public Pharmacy? NearestPharmacy { get; set; }
+}
+/*
 public class InfrastructureInfo
 {
     public int? InfrastructureInfoId { get; set; }
@@ -12,4 +42,4 @@ public class InfrastructureInfo
     public string Facilities { get; set; } = string.Empty;
     
     public Building? Building { get; set; }
-}
+}*/
