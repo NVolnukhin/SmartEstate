b26c1e846072ecbc34900da5a2d6d2d038de9e05
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-04 17:29:19 +0300

    update paths

--- backend/parser/geodistance_calculator/constants.py
+++ backend/parser/geodistance_calculator/constants.py
@@ -1,11 +1,10 @@
 import os
 
 EARTH_RADIUS = 6371.0  # Радиус Земли в км
-DATA_DIR = "secondary_data"
 
 FILE_PATHS = {
-    'kindergarten': os.path.join(DATA_DIR, 'kindergarten_parse_result.csv'),
-    'school': os.path.join(DATA_DIR, 'school_parse_result.csv'),
-    'shop': os.path.join(DATA_DIR, 'shop_parse_result.csv'),
-    'pharmacy': os.path.join(DATA_DIR, 'pharmacy_parse_result.csv')
+    'kindergarten': '../kindergarten_parse_result.csv',
+    'school': '../school_parse_result.csv',
+    'shop': '../shop_parse_result.csv',
+    'pharmacy': '../pharmacy_parse_result.csv'
 }
