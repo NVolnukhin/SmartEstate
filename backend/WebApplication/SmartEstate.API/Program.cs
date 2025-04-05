8b52e9faa45ecafa7d2832038408a119157a17df
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-05 17:17:21 +0300

    updated Program.cs

--- backend/WebApplication/SmartEstate.API/Program.cs
+++ backend/WebApplication/SmartEstate.API/Program.cs
@@ -1,5 +1,15 @@
+using System.Text;
 using DatabaseContext;
+using DatabaseContext.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Presentation.Endpoints;
+using SmartEstate.Application.Interfaces;
+using SmartEstate.Application.Services;
+using SmartEstate.DataAccess.Repositories;
+using SmartEstate.Infrastructure;
 using SmartEstate.Routing.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +18,28 @@ builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<IJwtProvider, JwtProvider>();
+builder.Services.AddLogging();
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtOptions"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtOptions:Issuer"],
+            ValidAudience = builder.Configuration["JwtOptions:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtOptions:SecretKey"]))
+        };
+    });
+builder.Services.AddAuthorization();
 
 builder.Services.AddRoutingModule(builder.Configuration);
 
@@ -28,6 +60,8 @@ app.UseSwaggerUI(options =>
 
 app.UseHttpsRedirection();
 app.MapControllers();
+app.MapUsersEndpoint();
+
 
 app.MapGet("/", () => "Hello World! Navigation to /swagger for API documentation");
 
