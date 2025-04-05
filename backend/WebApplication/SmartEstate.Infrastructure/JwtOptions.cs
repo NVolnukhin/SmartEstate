f6f6356b6e63cffee804b27628cf5efc2d8e960a
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-05 17:16:26 +0300

    updated Jwt Provider

--- backend/WebApplication/SmartEstate.Infrastructure/JwtOptions.cs
+++ backend/WebApplication/SmartEstate.Infrastructure/JwtOptions.cs
@@ -3,7 +3,7 @@ namespace SmartEstate.Infrastructure;
 public class JwtOptions
 {
     public string SecretKey { get; set; } = string.Empty;
-    public int ExpiresHours { get; set; } 
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int ExpiresHours { get; set; }
 }
-
-//TODO: JWT options and jwt provider
--- backend/WebApplication/SmartEstate.Infrastructure/JwtProvider.cs
+++ backend/WebApplication/SmartEstate.Infrastructure/JwtProvider.cs
@@ -20,17 +20,26 @@ public class JwtProvider : IJwtProvider
     
     public string GenerateToken(User user)
     {
-        Claim[] claims = [new("userId", user.UserId.ToString())];
-        
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserId.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.UniqueName, user.Login),
+            new Claim(ClaimTypes.Name, user.Name ?? string.Empty)
+        };
+    
         var signingCredentials = new SigningCredentials(
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)), SecurityAlgorithms.HmacSha256);
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)), 
+            SecurityAlgorithms.HmacSha256);
 
         var token = new JwtSecurityToken(
+            issuer: _options.Issuer,
+            audience: _options.Audience,
             claims: claims,
-            signingCredentials: signingCredentials,
-            expires: DateTime.UtcNow.AddHours(_options.ExpiresHours)
+            expires: DateTime.UtcNow.AddHours(_options.ExpiresHours),
+            signingCredentials: signingCredentials
         );
-        
+    
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
