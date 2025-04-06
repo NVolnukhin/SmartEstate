d2f68c5a513e2a15428d2d5e0871e757246bc127
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-06 23:27:04 +0300

    update ServiceCollectionExtensions

--- backend/WebApplication/SmartEstate.API/Extensions/ServiceCollectionExtensions.cs
+++ backend/WebApplication/SmartEstate.API/Extensions/ServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@ using SmartEstate.Application.Interfaces;
 using SmartEstate.Application.Services;
 using SmartEstate.ApplicationServices;
 using SmartEstate.DataAccess.Repositories;
+using SmartEstate.Email;
 using SmartEstate.Infrastructure;
 
 namespace Presentation.Extensions;
@@ -12,14 +13,24 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IUsersRepository, UsersRepository>();
-        services.AddScoped<UserService>();
+        services.AddScoped<IUserService, UserService>();
         services.AddScoped<IPasswordHasher, PasswordHasher>();
         services.AddScoped<IJwtProvider, JwtProvider>();
         services.AddScoped<IFlatsRepository, FlatsRepository>();
         services.AddScoped<IUserPreferencesRepository, UserPreferencesRepository>();
         services.AddScoped<IUserPreferencesService, UserPreferencesService>();
         services.AddScoped<IFlatService, FlatService>();
+        services.AddTransient<IEmailService, SmtpEmailService>();
+        services.AddScoped<IPasswordRecoveryService, PasswordRecoveryService>();
+        services.AddScoped<IPasswordRecoveryTokenRepository, PasswordRecoveryTokenRepository>();
+        services.AddScoped<ITokenGenerator, TokenGenerator>();
+
         
+        services.AddControllers()
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.SuppressModelStateInvalidFilter = false;
+            });
         return services;
     }
 }
