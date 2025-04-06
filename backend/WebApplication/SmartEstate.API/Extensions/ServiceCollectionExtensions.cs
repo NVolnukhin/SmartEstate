a114fe50c27d06b819be37bb68594592215d489e
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-06 17:31:47 +0300

    updated service collection

--- backend/WebApplication/SmartEstate.API/Extensions/ServiceCollectionExtensions.cs
+++ backend/WebApplication/SmartEstate.API/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using DatabaseContext.Repositories;
 using SmartEstate.Application.Interfaces;
 using SmartEstate.Application.Services;
+using SmartEstate.ApplicationServices;
 using SmartEstate.DataAccess.Repositories;
 using SmartEstate.Infrastructure;
 
@@ -14,8 +15,11 @@ public static class ServiceCollectionExtensions
         services.AddScoped<UserService>();
         services.AddScoped<IPasswordHasher, PasswordHasher>();
         services.AddScoped<IJwtProvider, JwtProvider>();
-        services.AddScoped<FlatsRepository>();
-
+        services.AddScoped<IFlatsRepository, FlatsRepository>();
+        services.AddScoped<IUserPreferencesRepository, UserPreferencesRepository>();
+        services.AddScoped<IUserPreferencesService, UserPreferencesService>();
+        services.AddScoped<IFlatService, FlatService>();
+        
         return services;
     }
 }
