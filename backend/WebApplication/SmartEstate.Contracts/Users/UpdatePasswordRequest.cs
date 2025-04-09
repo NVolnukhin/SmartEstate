23abf578a739f9c502bdb3ab30a083759673f941
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-09 12:53:53 +0300

    update UpdatePasswordRequest record

--- backend/WebApplication/SmartEstate.Contracts/Users/UpdatePasswordRequest.cs
+++ backend/WebApplication/SmartEstate.Contracts/Users/UpdatePasswordRequest.cs
@@ -3,12 +3,8 @@ using System.ComponentModel.DataAnnotations;
 namespace Presentation.Contracts.Users;
 
 public record UpdatePasswordRequest(
-    [Required(ErrorMessage = "Требуется текущий пароль")]
+    [Required][Length(64, 64)]
     string CurrentPassword,
-
-    [Required(ErrorMessage = "Требуется новый пароль")]
-    [MinLength(8, ErrorMessage = "Пароль должен содержать минимум 8 символов")]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", 
-        ErrorMessage = "Пароль должен содержать цифры, заглавные и строчные буквы")]
+    [Required][Length(64, 64)]
     string NewPassword
 );
