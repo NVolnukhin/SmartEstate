d58c398860ec08e89f142e44ffc50ab48be066a4
Valeriy Bocharov <bocharovva@student.bmstu.ru>
2025-04-09 12:21:33 +0300

    deleting old files

--- backend/parser/graphhooper/location.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Location:
-    id: int
-    latitude: float
-    longitude: float
--- backend/parser/graphhooper/main.py
+++
@@ -1,7 +0,0 @@
-from calculator import WalkingTimeCalculator
-
-
-if __name__ == "__main__":
-
-    calculator = WalkingTimeCalculator()
-    calculator.process_buildings(batch_size=27)
