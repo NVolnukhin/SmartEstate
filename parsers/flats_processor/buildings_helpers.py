a61be3e98470f5543d2f1a38c05f8b04f75599b9
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-04-14 00:48:23 +0300

    update flats processor for integration to main.py

--- parsers/flats_processor/buildings_helpers.py
+++ parsers/flats_processor/buildings_helpers.py
@@ -1,13 +1,13 @@
-from geocoder_yandex import geocode_yandex
+from .geocoder_yandex import geocode_yandex
 import csv
 from datetime import datetime
-from storage.buildings_db_storage import find_developer_id
+from .storage.buildings_db_storage import find_developer_id
 
 def get_unique_buildings_info():
     """Собирает уникальные данные о зданиях из CSV."""
     building_data = set()
 
-    with open('../flats_parse_result.csv', 'r', encoding='utf-8') as file:
+    with open('/Users/nikitavolnuhin/ghostbusters/parsers/flats_parse_result.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter=';')
         next(reader)
         for row in reader:
@@ -39,25 +39,30 @@ def use_geocode_and_find_additional_info(buildings_set: set, db_params) -> list:
     geocoded_data = []
 
     for i, (floors_count, address, developer, year, residential_complex) in enumerate(buildings_set, 1):
-        coordinates = geocode_yandex(address)
+        try:
+            coordinates = geocode_yandex(address)
 
-        if not coordinates:
-            print(f"Ошибка геокодирования для адреса: {address}")
+            if not coordinates:
+                print(f"Ошибка геокодирования для адреса: {address}")
+                continue
+
+            developer_id = find_developer_id(developer, db_params)
+            status = get_construction_status(year)
+
+            geocoded_data.append({
+                'floors_count': floors_count,
+                'address': address,
+                'developer_id': developer_id,
+                'status': status,
+                'lat': coordinates[1],
+                'lon': coordinates[0],
+                'residential_complex': residential_complex
+            })
+
+            print(f"geocoded {i} building")
+
+        except Exception as e:
+            print(f"Ошибка при обработке здания {i} (адрес: {address}): {str(e)}")
             continue
-
-        developer_id = find_developer_id(developer, db_params)
-        status = get_construction_status(year)
-
-        geocoded_data.append({
-            'floors_count': floors_count,
-            'address': address,
-            'developer_id': developer_id,
-            'status': status,
-            'lat': coordinates[1],
-            'lon': coordinates[0],
-            'residential_complex': residential_complex
-        })
-
-        print(f"geocoded {i} bilding")
 
     return geocoded_data
